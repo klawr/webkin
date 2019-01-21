0e75ce1af2c178bb2eca2aa0b9be2f8ee6a2cacb
Kai Lawrence <mail@kailawrence.de>
2019-01-21 21:16:05 +0100

    Add angular material design and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTabsModule } from '@angular/material/tabs';
+
+import { ScrollDispatchModule } from '@angular/cdk/scrolling';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserAnimationsModule,
+        BrowserModule,
+        MatButtonModule,
+        MatDialogModule,
+        MatDividerModule,
+        MatGridListModule,
+        MatInputModule,
+        MatListModule,
+        MatSelectModule,
+        MatSidenavModule,
+        MatSliderModule,
+        MatTabsModule,
+        ScrollDispatchModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
