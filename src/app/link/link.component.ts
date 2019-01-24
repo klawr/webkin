c6d7c66fc82999543054fe6e4ace5ca0cf8f6d6f
Kai Lawrence <mail@kailawrence.de>
2019-01-24 15:21:25 +0100

    Add new Link to Link Component

--- src/app/link/link.component.ts
+++ src/app/link/link.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Link, JointId } from '../mech/mech.model';
+import { Link } from '../mech/mech.model';
 import { Store } from '@ngrx/store';
 import { AppState } from '../app.state';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { MutateLinkAction } from '../mech/mech.actions';
 
 @Component({
     selector: 'app-link',
@@ -13,27 +14,63 @@ import { map } from 'rxjs/operators';
 export class LinkComponent implements OnInit {
     @Input() link: Link
 
+    backup: Link;
+
     constructor(
         public readonly store: Store<AppState>
     ) { }
 
     // TODO: Preview angle should be aligned with absAngle
 
-    joints: Observable<string[]>;
-
+    joints: Observable<{ id: string, mpIds: Iterable<number> }[]>;
+    edgeLengths = [] as number[];
+    relAngles = [] as number[];
+    selected: string;
     addEdge() {
+        this.edgeLengths.push(0);
+        this.relAngles.push(0);
+        console.log(this.edgeLengths.length);
     }
 
-    abort() {
+    update(id: string, preview: number)
+    {
+        const mp = this.selected ? this.selected.split(' - ') : undefined;
+        this.store.dispatch(new MutateLinkAction(id, {
+            id: id,
+            absAngle: preview,
+            edgeLengths: this.edgeLengths,
+            relAngles: this.relAngles,
+            joint: this.selected ? {linkId: mp[0], mountId: +mp[1] } : undefined,
+        }))
+    }
+
+    abort()
+    {
+        this.store.dispatch(new MutateLinkAction(this.backup.id, this.backup));
     }
 
     ngOnInit()
     {
-        // Implement this as https://material.angular.io/components/menu/overview#nested-menu
         const links = this.store.select(l => l.mech.links);
         this.joints = links.pipe(map(links =>
-            Object.keys(links).flatMap(k =>
-                links[k].edgeLengths.map((_, i) => links[k].id+' '+i))));
-    }
+            Object.keys(links).map(link => {
+                return {
+                    id: link,
+                    mpIds: links[link].edgeLengths.map((_, i) => i)
+                }
+            })
+        ));
+        if (this.link) {
+            this.backup =  Object.assign({}, this.link);
 
+            this.selected = this.link.joint ? this.link.joint.linkId + ' - ' + this.link.joint.mountId : undefined;
+            this.edgeLengths = [...this.link.edgeLengths];
+            this.relAngles = [...this.link.relAngles];
+
+        }
+        else
+        {
+            this.edgeLengths.push(0);
+        }
+    }
 }
